500eb9a917e252c1cfe481133eebc3c09e437dd4
qihangqiu1994 <166778541+qihangqiu1994@users.noreply.github.com>
2024-04-12 04:49:39 +0200

    Add files via upload

---
+++ Lab_ Working with files in Jupyter Notebooks.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# - List popular languages for Data Science.
+# - Identify commonly used libraries in Data Science.
+# - Create a table of Data Science tools.
+# - Introduce arithmetic expression examples in Python.
+# - Convert minutes to hours in Python.
+
+# Some of the popular languages that Data Scientists use are: 1. Python 2. R 3. Jave
+
+# Some of the commonly used libraries used by Data Scientists include: 1. NumPy 2. Matplotlib 3. scikit-learn
+
+# Data Science Tools
+# 
+# Jupyter Notebook
+# 
+# RStudio
+# 
+# VS Code
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[3]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[5]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+minutes = 200
+hours = minutes / 60
+print (hours)
+
+
+# ## Author
+# Qihang Qiu
+
+# In[ ]:
+
+
+
+
